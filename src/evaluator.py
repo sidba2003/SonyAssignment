f45e5cdc2592b39d6c6c1afa2997f9fdd78ffa46
Siddharth Bahl <98782166+sidba2003@users.noreply.github.com>
2025-06-20 04:30:23 +0100

    Update evaluator.py

--- src/evaluator.py
+++ src/evaluator.py
@@ -183,10 +183,7 @@ class Evaluator:
                 case ')':
                     pre_evaluated_number, _ = characters_stack.pop(), characters_stack.pop()
                     characters_stack.append(cls.computed_value(characters_stack, pre_evaluated_number))
-                # in the case we have an integer...
-                # if the list is currently empty  or if the last seen character was an opening bracket then we add the integer and move on
-                # else we can safely assume we have an operater and another number before this...
-                # so we pop both of these off, carry out the correct computation and append the result onto the stack
+                # in the case we have an integer
                 case _:
                     characters_stack.append(cls.computed_value(characters_stack, character))
         
